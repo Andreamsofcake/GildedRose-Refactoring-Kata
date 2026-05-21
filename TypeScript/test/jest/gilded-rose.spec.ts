fbd9d785a52f888f646c4f136647e4958062a0f0
Andrea H McCurdy (Moulding) <andrea.moulding@gmail.com>
2026-05-21 12:04:12 -0700

    created only one unit test with edge cases

--- TypeScript/test/jest/gilded-rose.spec.ts
+++ TypeScript/test/jest/gilded-rose.spec.ts
@@ -1,6 +1,59 @@
+import { describe, it, expect, beforeEach, afterEach } from '@jest/globals';
 import { Item, GildedRose } from '@/gilded-rose';
 
-describe('Gilded Rose', () => {
+/**
+ * This unit test uses [Jest Snapshot](https://goo.gl/fbAQLP).
+ * 
+ * There are two test cases here with different styles:
+ * <li>"foo" is more similar to the unit test from the 'Java' version
+ * <li>"thirtyDays" is more similar to the TextTest from the 'Java' version
+ *
+ * I suggest choosing one style to develop and deleting the other.
+ */
+
+type GoldenMasterArgv = [
+  node: '<node>',
+  script: '<script>',
+  days: string
+];
+
+function setGoldenMasterArgv(days: number): void {
+  const argv: GoldenMasterArgv = ['<node>', '<script>', String(days)];
+  process.argv = argv;
+}
+
+describe('Gilded Rose Approval', () => {
+
+  let gameConsoleOutput: string;
+  let originalConsoleLog: (message: any) => void;
+  let originalProcessArgv: string[]
+
+  function gameConsoleLog(msg: string) {
+    if (msg) {
+      gameConsoleOutput += msg;
+    }
+    gameConsoleOutput += "\n";
+  }
+
+  beforeEach(() => {
+    // prepare capturing console.log to our own gameConsoleLog.
+    gameConsoleOutput = "";
+    originalConsoleLog = console.log;
+    console.log = gameConsoleLog;
+    originalProcessArgv = process.argv;
+  });
+
+  afterEach(() => {
+    // reset original console.log
+    console.log = originalConsoleLog;
+    process.argv = originalProcessArgv;
+  });
+
+  it('quality never goes below 0', () => {
+    const gildedRose = new GildedRose([new Item('foo', 10, 0)]);
+    const items = gildedRose.updateQuality();
+    expect(items[0].quality).toBe(0);
+  });
   it('updates a normal item', () => {
     const gildedRose = new GildedRose([
       new Item('foo', 5, 10),
@@ -11,12 +64,21 @@ describe('Gilded Rose', () => {
     );
   });
   it('returns an array of Items', () => {
-  const gildedRose = new GildedRose([
-    new Item('foo', 5, 10),
-  ]);
+    const gildedRose = new GildedRose([
+      new Item('foo', 5, 10),
+    ]);
 
-  const items: Item[] = gildedRose.updateQuality();
+    const items: Item[] = gildedRose.updateQuality();
 
-  expect(items[0]).toBeInstanceOf(Item);
-});
+    expect(items[0]).toBeInstanceOf(Item);
+  });
+
+  it('should thirtyDays', () => {
+    setGoldenMasterArgv(30);
+    process.argv = ["<node>", "<script>", "30"];
+    require('../golden-master-text-test.ts');
+       
+    expect(gameConsoleOutput).toMatchSnapshot();
+  });
+
 });
