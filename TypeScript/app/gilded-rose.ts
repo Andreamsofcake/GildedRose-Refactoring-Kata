4ef84a37657bb4a65b297e66aade0b4fcb9b49f2
Andrea H McCurdy (Moulding) <andrea.moulding@gmail.com>
2026-05-21 13:02:56 -0700

    updated types

--- TypeScript/app/gilded-rose.ts
+++ TypeScript/app/gilded-rose.ts
@@ -3,10 +3,10 @@ export class Item {
   sellIn: number;
   quality: number;
 
-  constructor(name, sellIn, quality) {
-    this.name = name;
-    this.sellIn = sellIn;
-    this.quality = quality;
+constructor(name: string, sellIn: number, quality: number) {
+  this.name = name;
+  this.sellIn = sellIn;
+  this.quality = quality;
   }
 }
 const AGED_BRIE = 'Aged Brie';
@@ -29,14 +29,14 @@ export class GildedRose {
         this.items[i].name !== BACKSTAGE_PASS
       ) {
         if (this.items[i].quality > 0) {
-          if (this.items[i].name != SULFURAS) {
+          if (this.items[i].name !== SULFURAS) {
             this.items[i].quality = this.items[i].quality - 1
           }
         }
       } else {
         if (this.items[i].quality < 50) {
           this.items[i].quality = this.items[i].quality + 1
-          if (this.items[i].name == BACKSTAGE_PASS) {
+          if (this.items[i].name === BACKSTAGE_PASS) {
             if (this.items[i].sellIn < 11) {
               if (this.items[i].quality < 50) {
                 this.items[i].quality = this.items[i].quality + 1
@@ -50,14 +50,14 @@ export class GildedRose {
           }
         }
       }
-      if (this.items[i].name != SULFURAS) {
+      if (this.items[i].name !== SULFURAS) {
         this.items[i].sellIn = this.items[i].sellIn - 1;
       }
       if (this.items[i].sellIn < 0) {
-        if (this.items[i].name != AGED_BRIE) {
-          if (this.items[i].name != BACKSTAGE_PASS) {
+        if (this.items[i].name !== AGED_BRIE) {
+          if (this.items[i].name !== BACKSTAGE_PASS) {
             if (this.items[i].quality > 0) {
-              if (this.items[i].name != SULFURAS) {
+              if (this.items[i].name !== SULFURAS) {
                 this.items[i].quality = this.items[i].quality - 1
               }
             }
